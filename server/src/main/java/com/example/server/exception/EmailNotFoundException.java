8c6c7d612e85b1c18c0ff6459ade079aebde6642
TerryNguyen1403 <giangnht1403@gmail.com>
2026-09-12 10:43:02 +0700

    feat: create login validation exceptions

--- server/src/main/java/com/example/server/exception/EmailNotFoundException.java
+++ server/src/main/java/com/example/server/exception/EmailNotFoundException.java
@@ -1,4 +1,7 @@
 package com.example.server.exception;
 
-public class EmailNotFoundException {
+public class EmailNotFoundException extends RuntimeException {
+    public EmailNotFoundException(String message) {
+        super(message);
+    }
 }
