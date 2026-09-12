21d1de04bdf4f090acc987c10d3cda907d6a1ffd
TerryNguyen1403 <giangnht1403@gmail.com>
2026-09-12 10:41:19 +0700

    feat: Create UserLoginDTO

--- server/src/main/java/com/example/server/dto/UserLoginDTO.java
+++ server/src/main/java/com/example/server/dto/UserLoginDTO.java
@@ -1,4 +1,14 @@
 package com.example.server.dto;
 
-public record UserLoginDTO() {
+import com.example.server.validation.StrongPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserLoginDTO(
+        @NotBlank(message = "Email không được để trống")
+        @Email(message = "Vui lòng nhập email")
+        String email,
+        @NotBlank(message = "Mật khẩu không được để trống")
+        @StrongPassword
+        String password) {
 }
