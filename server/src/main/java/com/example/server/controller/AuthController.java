eabe4007efa2db46a0e96d66d17b3896776fbe84
TerryNguyen1403 <giangnht1403@gmail.com>
2026-09-12 10:45:35 +0700

    feat: Create login bussiness logic flow

--- server/src/main/java/com/example/server/controller/AuthController.java
+++ server/src/main/java/com/example/server/controller/AuthController.java
@@ -1,4 +1,24 @@
 package com.example.server.controller;
 
+import com.example.server.dto.UserLoginDTO;
+import com.example.server.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+  private final AuthService authService;
+
+  public AuthController(AuthService authService) {
+    this.authService = authService;
+  }
+
+  @PostMapping("/log-in")
+  public String login(@Valid @RequestBody UserLoginDTO request) {
+    return authService.login(request);
+  }
 }
--- server/src/main/java/com/example/server/security/JwtTokenProvider.java
+++ server/src/main/java/com/example/server/security/JwtTokenProvider.java
@@ -1,4 +1,51 @@
 package com.example.server.security;
 
+import com.example.server.entity.User;
+import com.example.server.entity.enums.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
 public class JwtTokenProvider {
+    private final SecretKey secretKey;
+    private final long expirationMs;
+
+    // Constructor injection
+    public JwtTokenProvider(
+            @Value("${app.jwt.secret}") String secretKey,
+            @Value("${app.jwt.expiration-ms}") long expirationMs
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.expirationMs = expirationMs;
+    }
+
+    public String generateToken(String email, Role role) {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + expirationMs);
+
+        return Jwts
+                .builder()
+                .subject(email)
+                .claim("role", role)
+                .signWith(secretKey)
+                .issuedAt(now)
+                .expiration(expiration)
+                .compact();
+    }
+
+    public Claims parseClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 }
