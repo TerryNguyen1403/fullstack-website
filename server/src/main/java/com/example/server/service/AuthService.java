52d3efff8931fb9c152707048ece52279b648a86
TerryNguyen1403 <giangnht1403@gmail.com>
2026-09-14 18:05:21 +0700

    feat: login authentication bussiness logic & controller

--- server/src/main/java/com/example/server/service/AuthService.java
+++ server/src/main/java/com/example/server/service/AuthService.java
@@ -1,37 +1,54 @@
 package com.example.server.service;
 
-import com.example.server.dto.UserLoginDTO;
-import com.example.server.entity.User;
-import com.example.server.exception.EmailNotFoundException;
-import com.example.server.exception.InvalidPasswordException;
+import com.example.server.dto.LoginRequestDTO;
+import com.example.server.dto.LoginResponseDTO;
+import com.example.server.entity.enums.Role;
 import com.example.server.repository.UserRepository;
 import com.example.server.security.JwtTokenProvider;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @Service
 public class AuthService {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthService(UserRepository userRepository, JwtTokenProvider jwtTokenProvider, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
+    public AuthService(JwtTokenProvider jwtTokenProvider, AuthenticationManager authenticationManager) {
         this.jwtTokenProvider = jwtTokenProvider;
-        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
     }
 
-    public String login(UserLoginDTO request) {
-        // Throw exception nếu user không tồn tại / chưa đăng ký
-        User exist = userRepository.findByEmail(request.email()).orElseThrow(() -> new EmailNotFoundException("Email không tồn tại"));
+    public LoginResponseDTO login(LoginRequestDTO request) {
+        // 1. Đóng gói email + password thành một "yêu cầu xác thực"
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(request.email(), request.password());
 
-        // Throw exception nếu mật khẩu không khớp
-        if (!passwordEncoder.matches(request.password(), exist.getPassword()))
-          throw new InvalidPasswordException("Sai mật khẩu");
+        // 2. Giao cho AuthenticationManager kiểm tra,
+        //      nếu sai throw BadCredentialException
+        Authentication authentication = authenticationManager.authenticate(authToken);
 
-      // Generate token
-      return jwtTokenProvider.generateToken(request.email(), exist.getRole());
+        // 3. Xác thực thành công, lấy user đã được nạp
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        assert userDetails != null;
+        String authority = userDetails.getAuthorities().stream()
+                .findFirst().map(GrantedAuthority::getAuthority).orElseThrow();
+
+        Role role = Role.valueOf(authority.replace("ROLE_", ""));
+
+        // 4. Sinh Jwt Token
+        String jwt = jwtTokenProvider.generateToken(userDetails.getUsername(), role);
+        long expiresIn = jwtTokenProvider.getExpirationMs();
+
+        return new LoginResponseDTO(jwt, expiresIn);
+    }
+
+    public boolean validateToken(String token) {
+      return jwtTokenProvider.validateToken(token);
     }
 }
