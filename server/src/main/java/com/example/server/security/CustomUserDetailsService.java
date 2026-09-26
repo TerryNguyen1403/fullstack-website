146c047e4ed98bb93329f7cff7fdc90cdd85108b
TerryNguyen1403 <giangnht1403@gmail.com>
2026-09-26 18:50:20 +0700

    fix: fix LazyInitializationException

--- server/src/main/java/com/example/server/security/CustomUserDetailsService.java
+++ server/src/main/java/com/example/server/security/CustomUserDetailsService.java
@@ -1,35 +1,35 @@
 package com.example.server.security;
 
-import com.example.server.entity.User;
-import com.example.server.repository.UserRepository;
+import java.util.List;
+
 import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.example.server.entity.User;
+import com.example.server.repository.UserRepository;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+	public CustomUserDetailsService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
-    @Override
-    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username).orElseThrow(() ->
-                new UsernameNotFoundException("Email không tồn tại: " + username));
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Email không tồn tại: " + username));
+		String roleName = user.getRole().getRoleName().name();
 
-        return org.springframework.security.core.userdetails.User
-                .withUsername(user.getEmail())
-                .password(user.getPassword())
-                .authorities(List.of(
-                        new SimpleGrantedAuthority("ROLE_" + user.getRole())
-                ))
-                .build();
-    }
+		return org.springframework.security.core.userdetails.User.withUsername(user.getEmail())
+				.password(user.getPassword()).authorities(List.of(new SimpleGrantedAuthority("ROLE_" + roleName)))
+				.build();
+	}
 }
